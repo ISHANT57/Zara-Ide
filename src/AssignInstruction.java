29be0336b75d45385f5658340dd3ecf2fa9c833a
ISHANT57 <ishantbhoyar59@gmail.com>
2026-03-31 13:21:17 +0530

    bug fix  AssignInstruction

--- src/AssignInstruction.java
+++ src/AssignInstruction.java
@@ -1,4 +1,15 @@
+public class AssignInstruction implements Instruction {
+    private String varName;
+    private Expression value;
 
-public class AssignInstruction {
+    public AssignInstruction(String varName, Expression value) {
+        this.varName = varName;
+        this.value = value;
+    }
 
+    @Override
+    public void execute(Environment env) {
+        Object result = value.evaluate(env);
+        env.set(varName, result);
+    }
 }
